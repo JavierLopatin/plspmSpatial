ffd691efd8e6c1ea899cfd2de3b94d4a5ea2b421
JavierLopatin <javierlopatin@gmail.com>
2020-04-27 17:08:53 -0400

    add LV accuracies

--- plspmPredict.R
+++ plspmPredict.R
@@ -143,6 +143,27 @@ plspmPredict <- function(pls, dat)
   # Estimate Factor Scores from Inner Path and complete Matrix
   fscores <- fscores + fscores %*% t(path_coef)
 
+  # obtain accuracy metrics from the predicted scores
+  fit_scores <- matrix(ncol=num_endo, nrow = 4, byrow = T)
+  colnames(fit_scores) <- unique(smMatrix[,2])
+  rownames(fit_scores) <- c('r_square','RMSE','nRMSE','bias')
+  for (i in 1:num_endo) {
+    tryCatch({
+      # index for endo LV
+      k1 <- unique(smMatrix[,2])[i]
+      # index for indep LVs
+      k2 <- smMatrix[,1] [ which(smMatrix[,2] == k1) ]
+
+      lm = lm(fscores_global[,k1] ~ fscores_global[,k2])
+      p = predict(lm)
+      fit_scores[1,i] = cor(fscores_global[,k1], p, method="pearson")
+      fit_scores[2,i] = sqrt(mean((fscores_global[,k1] - p)^2))
+      fit_scores[3,i] = (RMSE_scores[1,i]/( max(fscores_global[,k1]) - min(fscores_global[,k1]) ))*100
+      fit_scores[4,i] =  1-coef( lm(p~fscores_group2[,k1]-1) )
+
+      }, error = function(e) { skip_to_next <<- TRUE})
+  }
+
   # Predict Measurements with loadings
   predictedMeasurements <- fscores %*% t(outer_loadings)
 
@@ -154,58 +175,36 @@ plspmPredict <- function(pls, dat)
   # Calculating the measurement residuals and accuracies if validation data is provided
   if (!is.na(sum( match( eMeasurements, colnames(dat) ) ))){
 
-      # measurement variables data
-      mmData = dat[, eMeasurements]
+    # measurement variables data
+    mmData = dat[, eMeasurements]
 
-      # get residuals
-      mmResiduals <- dat[,resMeasurements] - predictedMeasurements[,resMeasurements]
+    # get residuals
+    mmResiduals <- dat[,resMeasurements] - predictedMeasurements[,resMeasurements]
 
-      # get r-sqaure values
-      r_square <- matrix(ncol = length(resMeasurements), nrow = 1, byrow = T)
-      colnames(r_square) <- resMeasurements
-      for (i in 1:length(resMeasurements)){
-        r_square[,i] <- cor(dat[,resMeasurements[i]], predictedMeasurements[,resMeasurements[i]], method="pearson")
-      }
-
-      # get RMSE values
-      RMSE <- matrix(ncol = length(resMeasurements), nrow = 1, byrow = T)
-      colnames(RMSE) <- resMeasurements
-      for (i in 1:length(resMeasurements)){
-        RMSE[,i] <- sqrt(mean((dat[,resMeasurements[i]] - predictedMeasurements[,resMeasurements[i]])^2))
-      }
-
-      # get normalize-RMSE values
-      nRMSE <- matrix(ncol = length(resMeasurements), nrow = 1, byrow = T)
-      colnames(nRMSE) <- resMeasurements
-      for (i in 1:length(resMeasurements)){
-        nRMSE[,i] <- (RMSE[,i]/( max(dat[,resMeasurements[i]]) - min(dat[,resMeasurements[i]]) ))*100
-      }
-
-      # get bias values
-      bias <- matrix(ncol = length(resMeasurements), nrow = 1, byrow = T)
-      colnames(bias) <- resMeasurements
-      for (i in 1:length(resMeasurements)){
-        lm = lm(predictedMeasurements[,resMeasurements[i]] ~ dat[,resMeasurements[i]]-1)
-        bias[,i] <- 1-coef(lm)
-      }
+    # get accuracies of measurement predictions
+    fit_measurements <- matrix(ncol=length(resMeasurements), nrow = 4, byrow = T)
+    colnames(fit_measurements) <- resMeasurements
+    rownames(fit_measurements) <- c('r_square','RMSE','nRMSE','bias')
+    for (i in 1:length(resMeasurements)){
+      fit_measurements[1,i] <- cor(dat[,resMeasurements[i]], predictedMeasurements[,resMeasurements[i]], method="pearson")
+      fit_measurements[2,i] <- sqrt(mean((dat[,resMeasurements[i]] - predictedMeasurements[,resMeasurements[i]])^2))
+      fit_measurements[3,i] <- (RMSE[,i]/( max(dat[,resMeasurements[i]]) - min(dat[,resMeasurements[i]]) ))*100
+      lm = lm(predictedMeasurements[,resMeasurements[i]] ~ dat[,resMeasurements[i]]-1)
+      fit_measurements[4,i] <- 1-coef(lm)
+    }
   } else {
     mmData = dat[,pMeasurements]
     residuals = NA
-    r_square = NA
-    RMSE = NA
-    nRMSE = NA
-    bias = NA
+    fit_measurements = NA
   }
 
   # Prepare return Object
   predictResults <- list(mmData = mmData,
                          mmPredicted = predictedMeasurements[,resMeasurements],
                          mmResiduals = mmResiduals,
+                         mmfit = fit_measurements,
                          Scores = fscores,
-                         r_square = r_square,
-                         RMSE = RMSE,
-                         nRMSE = nRMSE,
-                         bias = bias)
+                         fitScores = fit_scores)
 
   class(predictResults) <- "plspmPredict"
   return(predictResults)
